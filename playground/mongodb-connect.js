a0b77f2d40d3ab7f7dc9d2bfbfd4c3f8487b5951
Arun Kumar <arun.kasireddi@gmail.com>
2017-09-12 16:39:16 +0100

    Initial commit for todo api

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,8 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server', err);
+    }
+    console.log('Connected to MongoDB server');
+});
