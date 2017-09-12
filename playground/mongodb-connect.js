e19bed0d34c4f6a234c440904d8bc6edb31d25c9
Arun Kumar <arun.kasireddi@gmail.com>
2017-09-12 16:51:33 +0100

    Added code to create todo and user collection using mongo api

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -5,4 +5,27 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
         return console.log('Unable to connect to MongoDB server', err);
     }
     console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    // db.collection('Users').insertOne({
+    //     name: 'Arun Kumar',
+    //     age: 28,
+    //     location: 'Dublin'
+    // }, (err, result) =>{
+    //     if(err){
+    //         return console.log('Unable to insert user', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    db.close();
 });
