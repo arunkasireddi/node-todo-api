873131ad0560663844313deb5c6ab27b7888d465
Arun Kumar <arun.kasireddi@gmail.com>
2017-09-13 11:32:33 +0100

    Added code to find documents using objectIDs and properties

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,5 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     if (err) {
@@ -20,11 +21,11 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     name: 'Arun Kumar',
     //     age: 28,
     //     location: 'Dublin'
-    // }, (err, result) =>{
-    //     if(err){
+    // }, (err, result) => {
+    //     if (err) {
     //         return console.log('Unable to insert user', err);
     //     }
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
     // });
 
     db.close();
