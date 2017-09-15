dc4b9597fdab56057be05d0930068ab629ee2c5e
arunkasireddi <arun.kasireddi@gmail.com>
2017-09-15 19:35:39 +0100

    Added simple version of user model. v0.1

--- server/server.js
+++ server/server.js
@@ -99,6 +99,20 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    var user = new User(body);
+    user.save().then((user) => {
+        res.status(200).send(user);
+    
+    }).catch((e)=>{
+        res.status(400).send(e);
+    });
+});
+
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
